4b5d8ec318add253da423873131fa307b35a5677
luoxuehuan <18868825313@163.com>
2016-05-04 16:42:03 +0800

    修改区别

--- src/com/java/collection/TestHashTable.java
+++ src/com/java/collection/TestHashTable.java
@@ -24,26 +24,35 @@ public class TestHashTable {
 	/**
 	 * 区别：
 	 * 
-	 * 1.hashtable是同步的：
-	 * public synchronized V put(K key, V value) 
-	 * hashmap 由于非线程安全，效率上可能高于Hashtable
+	 * 【1】.HashTable是同步的：
 	 * 
-	 * 2.
+	 * ---HashTable：
+	 * public synchronized V put(K key, V value) 
+	 * ---HashMap：
+	 * public V put(K key, V value)
+	 * 
+	 * HashMap 由于非线程安全，效率上可能高于Hashtable
+	 * 
+	 * 【2】.
 	 * hashmap的key 可以有一个为null,值可以为空
 	 * hashtable的key和value都不能为null：
 	 * 值不能为空：
-	 *  if (value == null) {
-            throw new NullPointerException();
-        }
+	 * ---HashTable
+	 * if (value == null) { throw new NullPointerException();}
+	 * ---HashMap
 	 * 
 	 * 
-	 * 3.父类不同。
+	 * 
+	 * 【3】.父类不同。
+	 * 
+	 * ---HashMap
 	 * public class HashMap<K,V> extends AbstractMap<K,V>
-	 * 
+	 * ---Hashtable
 	 * public class Hashtable<K,V> extends Dictionary<K,V>
 	 * 
-	 * 使用建议：
+	 * 【4】.使用建议：
 	 *  需要同步时,用hashtable。反之用hashmap.但是必要的时候hashmap也可以同步。
+	 *  Hashtable和HashMap采用的hash/rehash算法都大概一样，所以性能不会有很大的差异
 	 * 
 	 */
 
