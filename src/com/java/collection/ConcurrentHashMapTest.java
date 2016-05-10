db6aad8221c2d008c077a4bb09f4b3af53c44fc5
luoxuehuan <18868825313@163.com>
2016-05-10 14:37:10 +0800

    update name

--- src/com/java/collection/ConcurrentHashMapTest.java
+++ src/com/java/collection/ConcurrentHashMapTest.java
@@ -3,7 +3,7 @@ package com.java.collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class TestConcurrentHashMap {
+public class ConcurrentHashMapTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
