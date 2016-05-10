8a9a709147ca6b7537b422e130b95576c596ed85
luoxuehuan <18868825313@163.com>
2016-05-10 14:39:48 +0800

    11

--- src/com/java/collection/ConcurrentHashMapTest.java
+++ src/com/java/collection/ConcurrentHashMapTest.java
@@ -7,7 +7,7 @@ public class ConcurrentHashMapTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Map map = new ConcurrentHashMap();
+		Map map = new ConcurrentHashMap();//和hashtable类似
 	}
 
 }
