1ef05e4df23ebec9850aab2a133cceae1fb87b4b
luoxuehuan <18868825313@163.com>
2016-06-22 13:16:44 +0800

    1

--- src/com/java/String/StringTest.java
+++ src/com/java/String/StringTest.java
@@ -4,7 +4,15 @@ public class StringTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String a = "abcdef";
+		String c = "dd";
+		char b = a.charAt(2);
+		//System.out.println(String.valueOf(b));
+		if('a'<'b'){
+			System.out.println("dd");
+		}
+		
+	
 	}
 
 }
---
+++ src/com/java/cache/redis/RedisJava.java
@@ -0,0 +1,21 @@
+package com.java.cache.redis;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  //Connecting to Redis server on localhost
+	      Jedis jedis = new Jedis("10.137.12.12",6379);
+	    
+	      System.out.println("Connection to server sucessfully");
+	      //check whether server is running or not
+	      System.out.println("Server is running: "+jedis.ping());
+	      
+	      jedis.set("tch", "sb");
+	      // Get the stored data and print it
+	      System.out.println("tch:: "+ jedis.get("tch"));
+	}
+
+}
