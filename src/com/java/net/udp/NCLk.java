de38d08164d379d0c47d2d028e3ace2cda139185
luoxuehuan <18868825313@163.com>
2016-07-15 09:57:28 +0800

    1

--- src/com/java/net/udp/NCLk.java
+++ src/com/java/net/udp/NCLk.java
@@ -10,24 +10,26 @@ public class NCLk {
 	public static void main(String[] args) {
 		BufferedReader bufr = new BufferedReader(new InputStreamReader(System.in));
 		String line = null;
-		while ((line = bufr.readLine()) != null) {
+		/*while ((line = bufr.readLine()) != null) {
 
 			byte[] buf = line.getBytes();
 
-			/*
+			
 			 * 192.168.1.0 是网段
 			 * 192.168.1.255 是网波
 			 * 发给192.168.1.255就是发给1-254
-			 */
+			 
 			DatagramPacket dp = new DatagramPacket(buf, buf.length, InetAddress.getByName("127.0.0.1"), 9999);
-			ds.send(dp);
+			
+			
+			dp.send(dp);
 
 			if ("886".equals(line)) {
 				break;
 			}
 
 		} 
-		ds.close();
+		ds.close();*/
 
 	}
 
