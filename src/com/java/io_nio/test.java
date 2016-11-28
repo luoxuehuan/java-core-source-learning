a40c2e69f1ec874f15920fd4d6f48a518a8ead8d
luoxuehuan <18868825313@163.com>
2016-11-28 14:29:22 +0800

    add and update

--- src/com/java/io_nio/test.java
+++ src/com/java/io_nio/test.java
@@ -8,7 +8,7 @@ public class test {
 		int i02 = 59;
 		Integer i03 =Integer.valueOf(59);
 		Integer i04 = new Integer(59);
-		System.out.println(i02==i04);//自动拆箱，比较的是值！xiugai
+		System.out.println(i02==i04);//自动拆箱，比较的是值！
 		System.out.println(i02==i03);//比较的是值。
 		System.out.println(i01==i02);//比较的是值。
 		System.out.println(i01==i04);
