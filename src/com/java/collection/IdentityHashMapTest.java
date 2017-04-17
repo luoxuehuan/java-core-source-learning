12410d405b91d3eee9136ffcf462554223ef1755
luoxuehuan <18868825313@163.com>
2017-04-17 17:52:18 +0800

    update

--- src/com/java/collection/IdentityHashMapTest.java
+++ src/com/java/collection/IdentityHashMapTest.java
@@ -23,7 +23,7 @@ public class IdentityHashMapTest {
 		 
 		
 	        //下面两行代码向IdentityHashMap对象添加两个key-value对  
-	        ihm.put(new String("语文"),89);  
+	        ihm.put(new String("语文"),89);
 	        ihm.put(new String("语文"),78);  
 	        //下面两行代码只会向IdentityHashMap对象添加一个key-value对  
 	        ihm.put("java",93);  
