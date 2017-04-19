bcf4afcd66c8fd1b4d32239c305e3739d4f05b47
罗学焕 <18868825313@163.com>
2017-04-19 11:19:55 +0800

    remove .idea

--- java-core/src/main/java/com/java/java8test/ATest.java
+++ java-core/src/main/java/com/java/java8test/ATest.java
@@ -4,30 +4,30 @@ public class ATest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		AImpl a = new AImpl();
-//		a.say(333);
-//		a.run();
+		AImpl a = new AImpl();
+		a.say(333);
+		a.run();
 	}
 
 }
 
-//interface A {
-//	 default void say(int a) {
-//	        System.out.println(a);
-//	    }
-//}
-//
-//interface B {
-//	    default void run() {
-//	        System.out.println("A.run");
-//	    }
-//
-//}
-//
-//class AImpl implements A,B{
-//
-//
-//	public void run(){
-//		System.out.println("ddd");
-//	}
-//}
+interface A {
+	 default void say(int a) {
+	        System.out.println(a);
+	    }
+}
+
+interface B {
+	    default void run() {
+	        System.out.println("A.run");
+	    }
+
+}
+
+class AImpl implements A,B{
+
+
+	public void run(){
+		System.out.println("ddd");
+	}
+}
